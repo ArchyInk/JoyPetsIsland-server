c8580ad7dcd632f5581facd9dda51991d47663a7
archy <archybool@gmail.com>
2024-09-01 15:22:12 +0800

    init

---
+++ app/src/test/java/com/biz/joypetsisland/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biz.joypetsisland.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
